e9dc4a90ade3f3d45903b7764866df515de9b90b
edoardo <edoardolenzi9@gmail.com>
2024-11-08 19:19:16 +0100

    modules

--- go/01.go
+++ go/01.go
@@ -3,7 +3,18 @@ package main
 import "fmt"
 
 func main() {
+
 	fmt.Println("hello world")
+
 }
 
-// go run main.go 
+// go version > 1.23
+// go run 01.go
+// go build 01.go
+
+// gofmt 01.go
+// go fmt 01.go
+// go mod init <name>
+// go fmt .
+
+// go install golang.org/x/tools/cmd/goimports@latest
